eb8ad7a7b6d13990de45133d707ec17eb57c3090
Fred Hsu <fredlhsu@gmail.com>
2024-05-03 19:29:07 -0700

    adding comments and cleaned up code

---
+++ mnist/README.md
@@ -0,0 +1,5 @@
+# Solving MNIST with simple NN
+
+## References
+
+[PyTorch MNIST Example](https://github.com/pytorch/examples/blob/main/mnist/main.py)
--- mnist/main.py
+++ mnist/main.py
@@ -13,14 +13,12 @@ class NeuralNetwork(nn.Module):
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28 * 28, 128),
             nn.ReLU(),
-            # nn.Linear(128, 128),
-            # nn.ReLU(),
+            # Output layer has 10 classes
             nn.Linear(128, 10),
         )
 
     def forward(self, x):
         x = self.flatten(x)
-        # x = torch.flatten(x)
         logits = self.linear_relu_stack(x)
         return F.log_softmax(logits, dim=1)
 
@@ -33,9 +31,12 @@ device = (
     else "cpu"
 )
 print(f"Using {device} device")
+
+# Move the model to GPU
 model = NeuralNetwork().to(device)
 
 transform = transforms.Compose([transforms.ToTensor()])
+
 train_ds = datasets.MNIST("../data", train=True, download=True, transform=transform)
 validation_ds = datasets.MNIST("../data", train=False, transform=transform)
 
@@ -45,24 +46,28 @@ validation_dl = torch.utils.data.DataLoader(validation_ds, batch_size=256, shuff
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 for x, y in train_dl:
+    # Need to move the data to GPU
     x = x.to(device)
     y = y.to(device)
     optimizer.zero_grad()
+
+    # Running the model with input data
     output = model(x)
+
+    # Using NLL as it is well suited for classification problems with C classes
+    # Also solved the dimension mismatch issue
     loss = F.nll_loss(output, y).backward()
     optimizer.step()
 
+# turn off gradient calculation for inference
 with torch.no_grad():
     for x, y in validation_dl:
         x = x.to(device)
         y = y.to(device)
         logits = model(x)
+        # Softmax because we are doing classification
         pred_probab = nn.Softmax(dim=1)(logits)
+        # Choose the class with highest probability
         y_pred = pred_probab.argmax(1)
-        # print((y_pred == y))
         acc = (y_pred == y).float().mean()
         print(f"Accuracy: {acc.item() * 100}%")
-
-    # print(f"Predicted class: {y_pred}")
-# X = torch.rand(1, 28, 28, device=device)
-# logits = model(X)
